8bea8695e5441c5a8b4400601d107e28f875866f
Irina Rymatskaya <irina.rymatskaya@gmail.com>
2025-10-29 22:01:43 +0300

    List and Set

--- src/by/lessons/task11/Main.java
+++ src/by/lessons/task11/Main.java
@@ -2,7 +2,7 @@ package by.lessons.task11;
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Ivan", "Inavov","Math");
-        Table<Integer,Person> table = new Table();
-
+        Student student = new Student("Ivan", "Inavov", "Math");
+        //Table<Integer,Person> table = new Table();
+    }
 }
